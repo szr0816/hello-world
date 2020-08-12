9cf93aed53e3e96ff520dae9d86935827cabba06
宋子睿 <69282873+szr0816@users.noreply.github.com>
2020-08-12 11:43:58 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a1=0;
+	int a2=0;
+	int num=0;
+	scanf("%d%d",&a1,&a2);
+	num=a1+a2;
+	printf("num=%d\n",num);
+	return 0;
+}   
